0ab72602cee926418e9fc1feedd1306e569c569a
AlexDzehil-BLR <70115369+AlexDzehil-BLR@users.noreply.github.com>
2020-09-24 17:45:54 +0300

    stagw 2/8: Let`s play a game

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,5 +1,16 @@
-def hello():
-    return print("""H A N G M A N
-The game will be available soon.""")
+# def hello():
+#     return print("""H A N G M A N
+# The game will be available soon.""")
+#
+# hello()
 
-hello()
+def game():
+    a = 'python'
+    print('H A N G M A N')
+    b = input('Guess the word: ')
+    if b == a:
+        print('You survived!')
+    else:
+        print('You lost!')
+
+game()
