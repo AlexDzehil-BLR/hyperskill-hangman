e9d38c03915fe61eca9e4aab4e6fe78a156750a3
AlexDzehil-BLR <70115369+AlexDzehil-BLR@users.noreply.github.com>
2020-10-11 17:38:12 +0300

    Stage 6/8: The value of life

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -7,23 +7,33 @@ def game():
     tryse = 0
     list_a = list(random.choice(map))
     list_b = ['-' for i in range(len(list_a))]
+    set_set = set()
     print('H A N G M A N')
-    print()
     while tryse < 8:
+        print()
+        if '-' not in list_b:
+            break
         print(''.join(list_b))
         letter = input('Input a letter: ')
+        if letter not in list_a:
+            print("That letter doesn't appear in the word")
+            tryse += 1
+        if letter in set_set and letter in list_a:
+            print('No improvements')
+            tryse += 1
+        if letter not in set_set and letter in list_a:
+            set_set.add(letter)
         if letter in list_a:
             for i in range(len(list_a)):
                 if list_a[i] == letter:
                     list_b[i] = letter
-            print()
-            tryse += 1
-        else:
-            print("That letter doesn't appear in the word")
-            print()
-            tryse += 1
 
-    print("""Thanks for playing!
-We'll see how well you did in the next stage""")
 
+    if '-' not in list_b:
+        print()
+        print(''.join(list_b))
+        print("""You guessed the word!
+You survived!""")
+    else:
+        print('You lost!')
 game()
