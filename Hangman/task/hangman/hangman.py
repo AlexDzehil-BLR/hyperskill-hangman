2f84bdc5ebd92e2a5cdc38d385bc626aa02c70dd
AlexDzehil-BLR <70115369+AlexDzehil-BLR@users.noreply.github.com>
2020-10-11 14:19:06 +0300

    Stage 5/8: Keep trying

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,16 +1,33 @@
-# def hello():
-#     return print("""H A N G M A N
-# The game will be available soon.""")
-#
-# hello()
+import random
+
+map = ['python', 'java', 'kotlin', 'javascript']
+
 
 def game():
-    a = 'python'
+    tryse = 0
+    a = random.choice(map)
+    list_a = list(a)
+    list_b = ['-' for i in range(len(a))]
     print('H A N G M A N')
-    b = input('Guess the word: ')
-    if b == a:
-        print('You survived!')
-    else:
-        print('You lost!')
+    print()
+    while tryse < 8:
+        print(''.join(list_b))
+        letter = input('Input a letter: ')
+        if letter in a:
+            for i in range(len(a)):
+                if list_b[i].isalpha():
+                    list_b[i] = list_a[i]
+                elif list_a[i] == letter:
+                    list_b[i] = letter
+            print()
+            tryse += 1
+        else:
+            print("That letter doesn't appear in the word")
+            print()
+            tryse += 1
+
+    print()
+    print("""Thanks for playing!
+We'll see how well you did in the next stage""")
 
 game()
