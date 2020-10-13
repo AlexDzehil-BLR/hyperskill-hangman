5059fd2e28f54be4f819673d2f5bea06dc7ab12c
AlexDzehil-BLR <70115369+AlexDzehil-BLR@users.noreply.github.com>
2020-10-13 12:29:19 +0300

    Stage 7/8: Error!

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -5,6 +5,11 @@ map = ['python', 'java', 'kotlin', 'javascript']
 
 def game():
     tryse = 0
+    alphabet = {
+                'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o',
+                'p', 'a', 's', 'd', 'f', 'g', 'h', 'j',
+                'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'
+                }
     list_a = list(random.choice(map))
     list_b = ['-' for i in range(len(list_a))]
     set_set = set()
@@ -15,19 +20,22 @@ def game():
             break
         print(''.join(list_b))
         letter = input('Input a letter: ')
-        if letter not in list_a:
-            print("That letter doesn't appear in the word")
-            tryse += 1
-        if letter in set_set and letter in list_a:
-            print('No improvements')
-            tryse += 1
-        if letter not in set_set and letter in list_a:
-            set_set.add(letter)
-        if letter in list_a:
-            for i in range(len(list_a)):
-                if list_a[i] == letter:
-                    list_b[i] = letter
-
+        if len(letter) == 0 or len(letter) > 1:
+            print('You should input a single letter')
+        elif not bool(alphabet.intersection(set(letter))):
+            print('Please enter a lowercase English letter')
+        else:
+            if letter not in list_a and letter not in set_set:
+                print("That letter doesn't appear in the word")
+                set_set.add(letter)
+                tryse += 1
+            elif letter in list_a and letter not in set_set:
+                set_set.add(letter)
+                for i in range(len(list_a)):
+                    if list_a[i] == letter:
+                        list_b[i] = letter
+            elif letter in set_set:
+                print("You've already guessed this letter")
 
     if '-' not in list_b:
         print()
@@ -36,4 +44,6 @@ def game():
 You survived!""")
     else:
         print('You lost!')
+
+
 game()
